a7af111e14c15577538f2fbc7bf80ae0c8ad4068
Joakim Hall <joakim.hall1@ingka.com>
2023-11-26 19:56:03 +0100

    fix: blogpost

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -1,35 +1,35 @@
 export default function BlogPage({ params: { locale } }: BasePageProps) {
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-4xl font-bold text-center my-6">My First Post</h1>
+<div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-center mb-6">Welcome to My Blog</h1>
 
-      <p className="text-lg mt-4">
-        Welcome to my blog! This is my very first post. I&apos;m excited to start this journey and share my thoughts, experiences, and knowledge with you.
+      <p className="text-lg">
+        Writing and expressing my thoughts doesn&apos;t come naturally to me, but I believe in challenging myself. This blog is a part of that challenge. Here, I&apos;ll share my journey, experiences, and learnings in various aspects of my life.
       </p>
 
-      <h2 className="text-2xl font-bold mt-6">Why I Started a Blog</h2>
+      <h2 className="text-2xl font-bold mt-6">Diverse Interests</h2>
       <p className="text-lg mt-4">
-        I&apos;ve always enjoyed writing and sharing information. Starting a blog seemed like a natural step for me to not only document my journey but also to connect with others who share similar interests.
-      </p>
-
-      <h2 className="text-2xl font-bold mt-6">What to Expect</h2>
-      <p className="text-lg mt-4">
-        In this blog, I plan to write about:
+        Expect to read about a mix of topics, including:
       </p>
       <ul className="list-disc list-inside mt-2">
-        <li>My experiences with web development, focusing on technologies like React, TypeScript, and Node.js.</li>
-        <li>Tips and tricks that I&apos;ve learned in my career as a software developer.</li>
-        <li>Personal development and learning strategies for continuous growth in the tech industry.</li>
+        <li>Insights and stories from my personal and professional life.</li>
+        <li>My fitness journey, workout routines, and health tips.</li>
+        <li>Adventures and experiences from my travels around the world.</li>
       </ul>
 
-      <h2 className="text-2xl font-bold mt-6">Stay Connected</h2>
+      <h2 className="text-2xl font-bold mt-6">Photography and Social Media</h2>
       <p className="text-lg mt-4">
-        I&apos;m looking forward to growing with my readers and building a community. Feel free to reach out to me on <a href="https://linkedin.com/in/yourprofile" className="text-blue-600 hover:text-blue-800">LinkedIn</a> or follow me on <a href="https://github.com/yourprofile" className="text-blue-600 hover:text-blue-800">GitHub</a> to stay updated with my latest projects and posts.
+        I also enjoy photography, so you&apos;ll find plenty of photos accompanying my blog posts. Don&apos;t forget to follow me on <a href="https://www.instagram.com/zokiioo/" className="text-blue-600 hover:text-blue-800">Instagram</a> for more visual content and updates.
       </p>
 
-      <p className="text-lg mt-4 mb-8">
-        Thank you for reading, and stay tuned for more!
+      <p className="text-lg mt-4">
+        I&apos;m excited to start this blogging journey and I hope you&apos;ll find something here that resonates with you. Thanks for joining me!
+      </p>
+      <p className="text-lg mt-4">
+        Feel free to reach out to me on <a href="https://www.linkedin.com/in/joakimhall/" className="text-blue-600 hover:text-blue-800">LinkedIn</a>
       </p>
     </div>
   )
 }
+
+// &apos;
